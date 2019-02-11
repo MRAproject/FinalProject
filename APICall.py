aa728b448d97acd8c7756980df30253bec3db958
matan <mrafinalproject@gmail.com>
2019-02-11 16:56:14 +0200

    new algorithems Marking Vehicle Number | push:matan

--- APICall.py
+++ APICall.py
@@ -3,6 +3,7 @@ from pprint import pprint
 
 API_TOKEN = "8b88fdaa4ba0c9a6bdc1c5cc853f9b89facb4aa5"
 
+
 def Work(imagePath):
     with open(imagePath, 'rb') as fp:
         response = requests.post(
@@ -10,6 +11,5 @@ def Work(imagePath):
             files=dict(upload=fp),
             headers={'Authorization': 'Token ' + API_TOKEN})
     pprint(response.json())
-    carNumber = response.json()['results'][0]['plate']
-    # print("carNumber = ", carNumber)
-    return carNumber
+    return response.json()['results'][0]
+    # carNumber = response.json()['results'][0]['plate']
--- imageProcessing.py
+++ imageProcessing.py
@@ -1,9 +1,19 @@
-import MarkingVehicleNumber
-import GetValueVehicleNumber
+import APICall
+import cv2
 
-imagePath = 'D:/tmp/imageM.jpeg'
+imagePath = 'D:/tmp/skoadaGarag.jpeg'
+data = APICall.Work(imagePath)
 
-MarkingVehicleNumber.Work(imagePath)
-VehicleNumber = GetValueVehicleNumber.Work(imagePath)
+# Marking Vehicle Number
+xmin = data['box']['xmin']
+xmax = data['box']['xmax']
+ymin = data['box']['ymin']
+ymax = data['box']['ymax']
+img = cv2.imread(imagePath, cv2.IMREAD_COLOR)
+cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 0), 3)
+img = cv2.resize(img, (800, 600))
+cv2.imshow('image', img)
 
-print(VehicleNumber)
+carNumber = data['plate']
+print("carNumber = ", carNumber)
+cv2.waitKey()
